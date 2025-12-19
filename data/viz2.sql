38ff6f898d1cc541c91d6380916a39fe9c6b5eaa
Henry <chehenry200@gmail.com>
2025-12-19 15:24:04 +0100

    added viz 3

--- data/viz2.sql
+++ data/viz2.sql
@@ -2,9 +2,24 @@ USE AdventureWorks2025
 
 select * from Production.ProductCategory
 
-
 select * from Production.ProductSubcategory
 
 select * from Production.Product
 
 select * from sales.SalesOrderDetail
+
+
+SELECT 
+    PC.Name AS CategoryName, 
+    SOD.OrderQty AS QuantitySold,
+    SOD.UnitPrice AS UnitPrice,
+    SOD.OrderQty * SOD.UnitPrice AS TotalSales,
+    COUNT(DISTINCT P.ProductID) as ProductCount,
+    SOD.LineTotal AS LineTotal
+FROM Production.ProductCategory AS PC 
+INNER JOIN Production.ProductSubcategory AS PS ON PC.ProductCategoryID = PS.ProductCategoryID  
+INNER JOIN Production.Product AS P ON PS.ProductSubcategoryID = P.ProductSubcategoryID
+INNER JOIN sales.SalesOrderDetail AS SOD ON P.ProductID = SOD.ProductID
+GROUP BY PC.Name
+ORDER BY ProductCount DESC;
+ 
--- data/viz3.sql
+++ data/viz3.sql
@@ -1,3 +1,14 @@
 USE AdventureWorks2025
 
 SELECT * FROM Sales.SalesOrderHeader
+
+
+
+SELECT
+FORMAT(SOD.OrderDate, 'yyyy-MM') AS OrderMonth,
+    SUM(SOD.SubTotal) AS TotalSales
+FROM Sales.SalesOrderHeader AS SOD
+GROUP BY
+    FORMAT(SOD.OrderDate, 'yyyy-MM')        
+ORDER BY
+    OrderMonth;
