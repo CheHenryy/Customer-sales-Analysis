b09a595295686bbe033589816356c410e47274c0
Henry <chehenry200@gmail.com>
2025-12-27 14:44:56 +0100

    I added viz 7

--- data/viz7.sql
+++ data/viz7.sql
@@ -4,3 +4,24 @@ SELECT * FROM Sales.SalesTerritory
 SELECT * FROM Sales.SalesOrderHeader
 SELECT * FROM Sales.Customer
 SELECT * FROM Sales.Store
+
+
+SELECT
+    st.Name AS Region,
+    CASE 
+        WHEN c.StoreID IS NOT NULL THEN 'Store'
+        WHEN c.PersonID IS NOT NULL THEN 'Individual'
+    END AS CustomerType,
+    SUM(soh.TotalDue) AS TotalSales,
+    COUNT(DISTINCT soh.SalesOrderID) AS OrderCount,
+    SUM(TotalDue)/COUNT(DISTINCT SalesOrderID) AS AvgOrderValue
+FROM Sales.SalesOrderHeader AS soh
+JOIN Sales.Customer AS c ON soh.CustomerID = c.CustomerID
+JOIN Sales.SalesTerritory AS st ON c.TerritoryID = st.TerritoryID
+GROUP BY st.Name,
+         CASE 
+             WHEN c.StoreID IS NOT NULL THEN 'Store'
+             WHEN c.PersonID IS NOT NULL THEN 'Individual'
+         END
+ORDER BY AVG(soh.TotalDue) DESC;
+
