bd227f4a5b8760504f5baad04aa8c21fe196e888
Henry <chehenry200@gmail.com>
2025-12-29 14:53:09 +0100

    Updated all diagrams

--- data/viz2.sql
+++ data/viz2.sql
@@ -11,12 +11,13 @@ select * from sales.SalesOrderDetail
 
 SELECT 
     PC.Name AS CategoryName, 
+    COUNT(SOD.OrderQty) AS NumberOfProductsSold,
     SUM(SOD.LineTotal) AS LineTotal,
     SUM(SOD.OrderQty * SOD.UnitPrice) AS TotalSales --verifying this figure with thr LineTotal
 FROM Production.ProductCategory AS PC 
 INNER JOIN Production.ProductSubcategory AS PS ON PC.ProductCategoryID = PS.ProductCategoryID  
 INNER JOIN Production.Product AS P ON PS.ProductSubcategoryID = P.ProductSubcategoryID
 INNER JOIN sales.SalesOrderDetail AS SOD ON P.ProductID = SOD.ProductID
-GROUP BY PC.Name     
-ORDER BY SUM(SOD.LineTotal) DESC, SUM(SOD.OrderQty * SOD.UnitPrice) DESC;
+GROUP BY PC.Name   
+ORDER BY  SUM(SOD.OrderQty * SOD.UnitPrice) DESC, NumberOfProductsSold DESC ;
 
